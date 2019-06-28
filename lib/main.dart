6f83515653794d9e44ea7acef66d0aec2e7bc14b
Ely Alamillo <ely0013@gmail.com>
2019-06-27 20:28:55 -0500

    fixed postioning of elements with stack and center

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
 
 import 'src/app.dart';
 
-void main() => runApp(App());
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(App());
+}
--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -40,22 +40,40 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
         title: Text('Animation'),
       ),
       body: GestureDetector(
-        child: buildAnimation(),
+        child: Center(
+          child: Stack(
+            children: <Widget>[
+              buildBox(),
+              buildCatAnimation(),
+            ],
+          ),
+        ),
         onTap: animateCat,
       ),
     );
   }
 
-  Widget buildAnimation() {
+  Widget buildCatAnimation() {
     return AnimatedBuilder(
       animation: catAnimation,
       builder: (context, child) {
-        return Container(
+        return Positioned(
           child: child,
-          margin: EdgeInsets.only(top: catAnimation.value),
+          bottom: catAnimation.value,
+          // restrict image size
+          right: 0.0,
+          left: 0.0,
         );
       },
       child: Cat(),
+    );
+  }
+
+  Widget buildBox() {
+    return Container(
+      height: 200.0,
+      width: 200.00,
+      color: Colors.brown,
     );
   }
 }
